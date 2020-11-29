c04d1315f512c49fe3b296ca9b41135292ba7b32
01shobitha <01shobitha@gmail.com>
2020-11-30 03:33:59 +0530

    styled login page

--- collab/views.py
+++ collab/views.py
@@ -13,9 +13,15 @@ def index(request):
 
 def details(request, username):
     name = request.user.username
-    
+    if request.user.is_authenticated:
+        message = 'Hai, this is ' + username
+        
+    else:
+        message = 'Login to view ' + username +'\'s page'
+
     context = {
-        'name': name, 
-    }
+            'name': name, 
+            'message': message,
+        }
 
     return render(request, 'details.html', context = context)
