c6cfe27f73eea548ddceef746856ad08765b0ae8
01shobitha <01shobitha@gmail.com>
2020-12-01 11:03:02 +0530

    registered models

--- collab/admin.py
+++ collab/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Language, Profile, LanguageUser, Project
 
 # Register your models here.
+admin.site.register(Language)
+admin.site.register(Project)
+admin.site.register(LanguageUser)
+admin.site.register(Profile)
